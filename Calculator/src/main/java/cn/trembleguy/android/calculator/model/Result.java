b1e7bb8238b51c44d7dd5a4887a28f4230c96ecd
trembleguy <trembleguy@trembleguydeMacBook-Pro.local>
2018-04-01 15:59:20 +0800

    finish model using js

---
+++ Calculator/src/main/java/cn/trembleguy/android/calculator/model/Result.java
@@ -0,0 +1,11 @@
+package cn.trembleguy.android.calculator.model;
+
+/**
+ * Created by trembleguy on 2018/3/31.
+ */
+public class Result {
+    public static final int SUCCESS = 1;
+    public static final int ERROR = 2;
+    public String msg;
+    public int type;
+}
